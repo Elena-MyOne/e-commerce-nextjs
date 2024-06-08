9be3e7006950af99628cc824b73bfd22fd7a966f
Elena-MyOne <yakovenko.ea72@gmail.com>
2024-06-08 19:02:44 -0400

    feat: add configuration files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  daisyui: {
+    themes: [
+      {
+        lightTheme: {
+          primary: '#c026d3',
+          secondary: '#fb7185',
+          accent: '#0076f4',
+          neutral: '#111309',
+          'base-100': '#fff',
+          info: '#00b6ff',
+          success: '#34d399',
+          warning: '#f59e0b',
+          error: '#dc2626',
+          body: {
+            'background-color': '#e3e6e6',
+          },
+        },
       },
-    },
+    ],
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
 export default config;
