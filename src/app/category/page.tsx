89990528d79089fe8e6198e7534408de96288c93
Elena-MyOne <yakovenko.ea72@gmail.com>
2024-06-25 00:17:47 -0400

    feat: add pagination component for category page

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,3 +1,4 @@
+import Pagination from "@/components/Pagination/Pagination";
 import ProductCard from "@/components/productCard/ProductCard";
 import { prisma } from "@/lib/db/prisma";
 
@@ -7,13 +8,18 @@ export default async function Category() {
   });
 
   return (
-    <section className="w-full">
-      <h1 className="mb-4 font-custom text-2xl font-bold">New Arrivals</h1>
-      <div className="flex flex-wrap gap-4">
-        {products.map((product) => (
-          <ProductCard product={product} key={product.id} />
-        ))}
+    <>
+      <section className="my-5 w-full sm:my-10">
+        <h1 className="mb-4 font-custom text-2xl font-bold">New Arrivals</h1>
+        <div className="flex flex-wrap gap-4">
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
+      </section>
+      <div className="flex w-full justify-center py-5 sm:py-10">
+        {/* <Pagination currentPage={2} totalPages={100} /> */}
       </div>
-    </section>
+    </>
   );
 }
