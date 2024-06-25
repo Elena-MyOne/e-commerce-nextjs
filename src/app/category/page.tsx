cfc222fd64cfc2a5da3df66e3b645b525456e0a1
Elena-MyOne <yakovenko.ea72@gmail.com>
2024-06-25 16:46:17 -0400

    feat: add pagination functionality

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -2,9 +2,24 @@ import Pagination from "@/components/Pagination/Pagination";
 import ProductCard from "@/components/productCard/ProductCard";
 import { prisma } from "@/lib/db/prisma";
 
-export default async function Category() {
+interface CategoryProps {
+  searchParams: { page: string };
+}
+
+export default async function Category({
+  searchParams: { page = "1" },
+}: CategoryProps) {
+  const currentPage = parseInt(page);
+  const productCardsPerPage = 9;
+
+  const totalItemCount = await prisma.product.count();
+
+  const totalPages = Math.ceil(totalItemCount / productCardsPerPage);
+
   const products = await prisma.product.findMany({
     orderBy: { id: "desc" },
+    skip: (currentPage - 1) * productCardsPerPage,
+    take: productCardsPerPage,
   });
 
   return (
@@ -18,7 +33,9 @@ export default async function Category() {
         </div>
       </section>
       <div className="flex w-full justify-center py-5 sm:py-10">
-        {/* <Pagination currentPage={2} totalPages={100} /> */}
+        {totalPages > 1 && (
+          <Pagination currentPage={currentPage} totalPages={totalPages} />
+        )}
       </div>
     </>
   );
